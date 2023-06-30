ae536d29b332daec155ae219318d0048a10c58bf
dzikryaji <dzikryaji07@gmail.com>
2023-06-30 20:54:04 +0700

    first commit

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BASEURL', 'http://localhost/SEACinema/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'sea_cinema');
